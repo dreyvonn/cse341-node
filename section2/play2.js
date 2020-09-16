f69f5676e512c0b7d50e60bfaa1107617d3c957b
Sam Haymond <hay16009@byui.edu>
2020-09-16 12:28:35 -0600

    finished js refresher

--- section2/play2.js
+++ section2/play2.js
@@ -6,11 +6,37 @@ const person = {
     }
 };
 
+// OBJECT DESTRUCTURING
+const printName = ({ name }) => {
+    console.log(name);
+}
+
+printName(person);
+
+const { name, age } = person;
+console.log(name, age);
+
 // person.greet();
 
+//ARRAY DESTRUCTURING
 const hobbies = ['Sports', 'Cooking'];
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1);
+
 // for (let hobby of hobbies) {
 //     console.log(hobby);
 // }
-console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
-console.log(hobbies);
+// console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies);
+// hobbies.push('Programming');
+
+//SPREAD OPERATOR
+// const copiedArray = [...hobbies];
+// console.log(copiedArray);
+
+// REST OPERATOR
+// const toArray = (...args) => {
+//     return args;
+// }
+
+// console.log(toArray(1, 2, 3, 4));
