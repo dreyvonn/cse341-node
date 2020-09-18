369a7eca33443c703605166f4b5e5fa516ba33da
Sam Haymond <hay16009@byui.edu>
2020-09-18 13:01:04 -0600

    clean up prove01

--- cse341-project-master/routes/prove01.js
+++ cse341-project-master/routes/prove01.js
@@ -7,18 +7,13 @@ router.use(bodyParser.urlencoded({extended: false}));
 
 router.use('/message',(req, res, next) => {
     res.render('pages/p01out', { 
-        title: 'Prove 01 Input', 
         userInput1: req.body.message,
         userInput2: req.body.message2,
-        path: '/p01out',
     });
 });
 
 router.use('/', (req, res, next) => {
-    res.render('pages/p01in', { 
-        title: 'Prove 01 Output', 
-        path: '/p01in',
-    });
+    res.render('pages/p01in');
 });
 
 module.exports = router;
