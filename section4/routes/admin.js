99ab71791c13a6310a3acb40448c4894f4347b20
Sam Haymond <hay16009@byui.edu>
2020-09-22 18:07:45 -0600

    pug stuff

--- section4/routes/admin.js
+++ section4/routes/admin.js
@@ -6,17 +6,20 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 //Middleware
 //Same path but GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
 //filter for only POST or GET requests (app.get())
 //Same path but POST
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    products.push({ title: req.body.title });
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- section4/routes/shop.js
+++ section4/routes/shop.js
@@ -3,11 +3,16 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html')); //dirname points to current folder location, ../ goes back on level in directories
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html')); //dirname points to current folder location, ../ goes back on level in directories
+    // console.log(adminData.products);
+
+    const products = adminData.products; 
+    res.render('shop', { prods: products, path: '/', pageTitle: 'Shop' });
 });
 
 module.exports = router;
