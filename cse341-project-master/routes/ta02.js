cc8f3d59918d6705299ec05e284ec11d0a4f474e
Sam Haymond <hay16009@byui.edu>
2020-09-24 09:27:53 -0600

    ta02

--- cse341-project-master/routes/ta02.js
+++ cse341-project-master/routes/ta02.js
@@ -1,7 +1,32 @@
 //TA02 PLACEHOLDER
 // Remember, you can make more of these placeholders yourself! 
 const express = require('express');
+const { products } = require('../../section4/routes/admin');
 const router = express.Router();
+
+const users = [];
+let removeFound = true;
+
+router.post('/addUser', (req, res, next) => {
+    users.push(req.body.userName);
+    res.redirect('/ta02');
+});
+
+router.post('/removeUser', (req, res, next) => {
+    const index = users.indexOf(req.body.removeUser);
+    if (index == -1) {
+        removeFound = false;
+    }
+    else {
+        for (let user of users) {
+            if (user === req.body.removeUser) {
+                users.splice(index, 1);
+            }
+        }
+        removeFound = true;
+    }
+    res.redirect('/ta02');
+});
 
 router.get('/',(req, res, next) => {
     res.render('pages/ta02', { 
@@ -9,6 +34,8 @@ router.get('/',(req, res, next) => {
         path: '/ta02', // For pug, EJS 
         activeTA03: true, // For HBS
         contentCSS: true, // For HBS
+        userArray: users,
+        remove: removeFound
     });
 });
 
