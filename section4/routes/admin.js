6336ad04747b717e4d5831c90b2c9b78871b0bb4
Sam Haymond <hay16009@byui.edu>
2020-09-29 17:25:06 -0600

    params

--- section4/routes/admin.js
+++ section4/routes/admin.js
@@ -2,16 +2,18 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 //Middleware
 //Same path but GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+router.get('/products', adminController.getProducts);
 
 //filter for only POST or GET requests (app.get())
 //Same path but POST
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
