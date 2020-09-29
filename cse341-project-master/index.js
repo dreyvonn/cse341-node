b0586b007bc606bbc9a791e64a61917cfce48b13
Sam Haymond <hay16009@byui.edu>
2020-09-29 09:41:17 -0600

    class activity 03

--- cse341-project-master/index.js
+++ cse341-project-master/index.js
@@ -25,6 +25,7 @@ const ta03Routes = require('./routes/ta03');
 const ta04Routes = require('./routes/ta04'); 
 const prove01Routes = require('./routes/prove01'); 
 const prove02Routes = require('./routes/prove02'); 
+const classActivities = require('./routes/classActivities/w03/routes');
 
 app.use(express.static(path.join(__dirname, 'public')))
    .set('views', path.join(__dirname, 'views'))
@@ -41,6 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')))
    .use('/ta04', ta04Routes)
    .use('/prove01', prove01Routes)
    .use('/prove02', prove02Routes)
+   .use('/classActivities/w03', classActivities)
    .get('/', (req, res, next) => {
      // This is the primary index, always handled last. 
      res.render('pages/index', {title: 'Welcome to my CSE341 repo', path: '/'});
