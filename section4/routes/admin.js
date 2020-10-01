ad8c55318ff48b662552753e816c3b0ce66b5ad5
Sam Haymond <hay16009@byui.edu>
2020-09-30 18:05:30 -0600

    dynamic routing

--- section4/routes/admin.js
+++ section4/routes/admin.js
@@ -10,10 +10,16 @@ const router = express.Router();
 //Same path but GET
 router.get('/add-product', adminController.getAddProduct);
 
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
 router.get('/products', adminController.getProducts);
 
 //filter for only POST or GET requests (app.get())
 //Same path but POST
 router.post('/add-product', adminController.postAddProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
