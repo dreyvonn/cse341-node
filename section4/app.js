d1291a46946171d0227d17f68c2bd022747f156e
Sam Haymond <hay16009@byui.edu>
2020-10-05 19:44:15 -0600

    nosql stuff

--- section4/app.js
+++ section4/app.js
@@ -4,24 +4,34 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-//parsing the request
-app.use(bodyParser.urlencoded({extended: false}));
-//allowing me to access path for css files
-app.use(express.static(path.join(__dirname, 'public')))
+const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+//   User.findById(1)
+//     .then(user => {
+//       req.user = user;
+//       next();
+//     })
+//     .catch(err => console.log(err));
+    next();
+});
 
 app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use(shopRoutes);
 
-//page not found error code 404
 app.use(errorController.get404);
 
-app.listen(3000);
+mongoConnect(() => {
+    app.listen(3000);
+});
