d67e0a03eb9c34511eabed612ae4164fc03e3b83
dreyvonn <38699352+dreyvonn@users.noreply.github.com>
2020-10-14 18:43:20 -0600

    Update app.js

--- section4/app.js
+++ section4/app.js
@@ -11,7 +11,7 @@ const flash = require('connect-flash');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI = 'mongodb+srv://shaymond:teancum1@cluster0.gsjik.mongodb.net/shop';
+const MONGODB_URI = '';
 
 const app = express();
 const store = new MongoDBStore({
